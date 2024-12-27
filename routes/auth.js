33b4e19e6c8a5e97abf2a59f57c79896de3d46e2
kanishkadeo27 <kanishkadeo92022@gmail.com>
2024-12-27 12:13:10 +0530

    login function completed

--- routes/auth.js
+++ routes/auth.js
@@ -21,4 +21,28 @@ router.post("/register", async (req, res) => {
     }
 });
 
+
+//LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const user =await User.findOne({ email: req.body.email });
+        !user && res.status(401).json("wrong password or username");
+
+        //if user exists validate password
+        var bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        var originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+        originalPassword !== req.body.password && res.status(401).json("wrong password or username");
+
+        //not to send password as we will store it in our local storage
+        const{password, ...info} = user._doc;
+
+        res.status(200).json(info);
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
 module.exports = router;
