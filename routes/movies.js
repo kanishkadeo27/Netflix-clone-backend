694247e9634dda7c490f938975b674e83d150e1e
kanishkadeo27 <kanishkadeo92022@gmail.com>
2024-12-27 15:30:59 +0530

    rest api movies crud operations completed

---
+++ routes/movies.js
@@ -0,0 +1,109 @@
+const router = require("express").Router();
+const Movie = require("../models/Movie");
+const CryptoJS = require("crypto-js");
+const verify = require("../verifyToken");
+
+
+//CREATE NEW MOVIE
+router.post('/', verify, async (req, res) => {
+    if (req?.user?.isAdmin) {
+        const newMovie = new Movie(req.body);
+        try {
+            const savedMovie = await newMovie.save();
+            res.status(200).json(savedMovie);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+    else {
+        res.status(403).json("you are not allowed to add movie")
+    }
+})
+
+//UPDATE EXISTING MOVIE
+router.put('/:id', verify, async (req, res) => {
+    if (req?.user?.isAdmin) {
+        const updatedMovie = await Movie.findByIdAndUpdate(req?.params?.id, { $set: req.body }
+            , {
+                new: true
+            }
+        );
+        try {
+            res.status(200).json(updatedMovie);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+    else {
+        res.status(403).json("you are not allowed to add movie")
+    }
+})
+
+ //DELETE
+router.delete('/:id', verify, async (req, res) => {
+    if (req?.user?.isAdmin) {
+        try {
+            await Movie.findByIdAndDelete(req?.params?.id);
+            res.status(200).json("Movie has been deleted...");
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+    else {
+        res.status(403).json("you cannot delete any movie")
+    }
+})
+
+ //GET1
+router.get('/find/:id', async (req, res) => {
+    try {
+        const movie = await Movie.findById(req?.params?.id);
+        res.status(200).json(movie);
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+)
+
+ //GET RANDOM MOVIE FOR FEATURED MOVIE
+router.get('/random', async (req, res) => {
+    const type = req.query.type;
+    let movie;
+    try {
+        if (type === 'series') {
+            movie = await Movie.aggregate([
+                { $match: { isSeries: true } },
+                { $sample: { size: 1 } },
+            ]);
+        } else {
+            movie = await Movie.aggregate([
+                { $match: { isSeries: false } },
+                { $sample: { size: 1 } },
+            ]);
+        }
+        res.status(200).json(movie);
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+)
+
+//GET ALL MOVIES
+router.get('/', verify, async (req, res) => {
+    if (req?.user?.isAdmin) {
+        try {
+            const movies = await Movie.find();
+            res.status(200).json(movies.reverse());
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+    else {
+        res.status(403).json("you are not allowed to see all users")
+    }
+})
+
+
+module.exports = router;
