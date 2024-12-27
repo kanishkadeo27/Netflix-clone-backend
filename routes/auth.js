05eb02bdf373c5a32aeb58ef57869b467bcf95cb
kanishkadeo27 <kanishkadeo92022@gmail.com>
2024-12-27 12:18:44 +0530

    login authentication using jwt done

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@
 const router = require("express").Router();
 const User = require('../models/User');
 const CryptoJS = require("crypto-js");
-
+const jwt = require('jsonwebtoken');
 
 //REGISTER
 //creating-post
@@ -34,10 +34,11 @@ router.post("/login", async (req, res) => {
 
         originalPassword !== req.body.password && res.status(401).json("wrong password or username");
 
+        const accessToken = jwt.sign({id:user._id , isAdmin:user.isAdmin} , process.env.SECRET_KEY , {expiresIn:"5d"})
         //not to send password as we will store it in our local storage
         const{password, ...info} = user._doc;
 
-        res.status(200).json(info);
+        res.status(200).json({...info,accessToken});
 
     } catch (error) {
         res.status(500).json(error);
